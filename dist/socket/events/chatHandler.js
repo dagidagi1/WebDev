1a73dfa5bcb5da5a5fef460fee2d8b285ba698b9
dagidagi1 <arkadya@ac.sce.ac.il>
2023-01-09 17:42:31 +0200

    a

--- dist/socket/events/chatHandler.js
+++ dist/socket/events/chatHandler.js
@@ -14,20 +14,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 const chat_model_1 = __importDefault(require("../../models/chat_model"));
 module.exports = (io, socket) => {
     const sendMessage = (payload) => __awaiter(void 0, void 0, void 0, function* () {
-        const message = new chat_model_1.default({
+        const msg = new chat_model_1.default({
             to: payload.to,
             message: payload.message,
             from: socket.data.user,
             time: Date.now()
         });
-        yield message.save();
-        io.to(message.to).emit("chat:message", { 'to': message.to, 'from': message.from, 'message': message.message });
+        yield msg.save();
+        io.to(msg.to).emit("chat:message", { 'to': msg.to, 'from': msg.from, 'message': msg.message });
     });
     const getMessages = (payload) => __awaiter(void 0, void 0, void 0, function* () {
-        const messagesFrom = yield chat_model_1.default.find({ "from": payload.id, "to": socket.data.user });
-        const messagesTo = yield chat_model_1.default.find({ "to": payload.id, "from": socket.data.user });
-        const messages = messagesFrom.concat(messagesTo);
-        socket.emit("chat:get_messages.response", messages);
+        const msgsFrom = yield chat_model_1.default.find({ "from": payload.id, "to": socket.data.user });
+        const msgsTo = yield chat_model_1.default.find({ "to": payload.id, "from": socket.data.user });
+        const msgs = msgsFrom.concat(msgsTo);
+        socket.emit("chat:get_messages.response", msgs);
     });
     socket.on("chat:send_message", sendMessage);
     socket.on("chat:get_messages", getMessages);
